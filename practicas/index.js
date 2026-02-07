91871e66bf5b47df12ba51af65caecf90fc06475
ITsF4CU <facundoleonel40@gmail.com>
2026-02-07 00:11:56 -0300

    Add files via upload

---
+++ practicas/index.js
@@ -0,0 +1,6 @@
+let texto = document.getElementById("texto");
+let boton = document.getElementById("cambiar");
+
+boton.addEventListener("click", function () {
+    texto.textContent = "Bueeeeenas";
+});
