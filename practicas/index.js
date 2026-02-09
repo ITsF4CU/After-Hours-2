bbd4dd6d129863b9aed64b6a1f0c58def163d30c
ITsF4CU <facundoleonel40@gmail.com>
2026-02-09 04:58:30 -0300

    Delete practicas directory

--- practicas/index.js
+++
@@ -1,6 +0,0 @@
-let texto = document.getElementById("texto");
-let boton = document.getElementById("cambiar");
-
-boton.addEventListener("click", function () {
-    texto.textContent = "Bueeeeenas";
-});
